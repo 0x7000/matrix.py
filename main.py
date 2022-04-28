ba61bb051cb68d529041581cb7505356abad3b2d
command <rekreb81@gmail.com>
2022-04-28 20:39:00 +0300

    xd2

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ import termcolor
 
 
 def main():
+    os.system("clear")
     size = str(TERM_SIZE).split(",")
     colm = int(size[0].split("=")[1])                       # sutun
     line = int(size[1].split("=")[1].removesuffix(")"))     # satır
     print('\033[?25l', end="")                              # hide cursor on bash
     sayac = 0
-    while sayac <= 50:
+    while 1:
         colm0 = random.randint(1, colm)
         line0 = random.randint(1, line-17)
         for i in range(1, 16):
-            pozisyon(colm0, line0+i)
-            sleep(0.02)
-        sayac += 1
+            pozisyon(line0+i, colm0)
+            for x in range(1, 8):
+                pozisyon(line0+x, colm0)
+                sleep(0.005)
+        if sayac == 200:
+            break
 
 
-def pozisyon(cc, ll):
+def pozisyon(ll, cc):
     # link = https://en.m.wikipedia.org/wiki/ANSI_escape_code
     print("\033[%d;%dH" % (ll, cc), end='')  # set cursor position
     text = chr(random.randint(65, 128))
