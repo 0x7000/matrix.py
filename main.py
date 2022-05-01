b5a4998675f2f81dd5d694dd1b31237aa0aa30b4
command <rekreb81@gmail.com>
2022-05-01 18:26:22 +0300

    xd2

--- main.py
+++ main.py
@@ -13,22 +13,28 @@ def main():
     print('\033[?25l', end="")                              # hide cursor on bash
     sayac = 0
     while 1:
+        sayac += 1
         colm0 = random.randint(1, colm)
         line0 = random.randint(1, line-17)
         for i in range(1, 16):
-            pozisyon(line0+i, colm0)
-            for x in range(1, 8):
-                pozisyon(line0+x, colm0)
-                sleep(0.005)
-        if sayac == 200:
+            pozisyon(line0+i, colm0, "m")
+            #sleep(0.01)
+            for x in range(1, 9):
+                pozisyon(line0+x, colm0+x, "x")
+                #sleep(0.001)
+        if sayac == 2000:
             break
 
 
-def pozisyon(ll, cc):
-    # link = https://en.m.wikipedia.org/wiki/ANSI_escape_code
-    print("\033[%d;%dH" % (ll, cc), end='')  # set cursor position
-    text = chr(random.randint(65, 128))
-    termcolor.cprint(text, "green")
+def pozisyon(ll, cc, w):
+    if w == "m":
+        # link = https://en.m.wikipedia.org/wiki/ANSI_escape_code
+        print("\033[%d;%dH" % (ll, cc), end='')  # set cursor position
+        text = chr(random.randint(65, 128))
+        termcolor.cprint(text, "green")
+    else:
+        print("\033[%d;%dH" % (ll, cc), end='')  # set cursor position
+        termcolor.cprint(" ", "green")
 
 
 TERM_SIZE = os.get_terminal_size()
